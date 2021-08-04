ab030e4334930ea5f27e124cca0e42bd44480ede
Akın Oslu <aknoslu3541@gmail.com>
2021-08-04 20:02:50 +0300

    www yönlendirmesi yapıldı

--- hera/settings.py
+++ hera/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.159.101.210','18.159.101.210:8000','127.0.0.1','127.0.0.1:8000']
+ALLOWED_HOSTS = ['18.159.101.210','18.159.101.210:8000','127.0.0.1','127.0.0.1:8000','www.herakomurculuk.com','herakomurculuk.com']
 
 # Application definition
 
